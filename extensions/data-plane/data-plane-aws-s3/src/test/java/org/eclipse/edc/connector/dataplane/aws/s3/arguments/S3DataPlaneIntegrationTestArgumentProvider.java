ad3431e8c86a221706bae9ac56809dce96147498
Máté Plutzer <mate.plutzer@t-systems.com>
2023-12-04 10:46:53 +0100

    chore: add missing copyright info

--- extensions/data-plane/data-plane-aws-s3/src/test/java/org/eclipse/edc/connector/dataplane/aws/s3/arguments/S3DataPlaneIntegrationTestArgumentProvider.java
+++ extensions/data-plane/data-plane-aws-s3/src/test/java/org/eclipse/edc/connector/dataplane/aws/s3/arguments/S3DataPlaneIntegrationTestArgumentProvider.java
@@ -1,3 +1,17 @@
+/*
+ *  Copyright (c) 2023 T-Systems International GmbH
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       T-Systems International GmbH
+ *
+ */
+
 package org.eclipse.edc.connector.dataplane.aws.s3.arguments;
 
 import org.junit.jupiter.api.Named;
