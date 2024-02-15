d104ed02aa4beb5ec81a008da01b0a66fbaf82df
yurimssilva <yuri.silva@ctw.bmwgroup.com>
2024-02-15 15:23:47 +0000

    test: adds test for the new behavior

--- extensions/data-plane/data-plane-aws-s3/src/test/java/org/eclipse/edc/connector/dataplane/aws/s3/TestFunctions.java
+++ extensions/data-plane/data-plane-aws-s3/src/test/java/org/eclipse/edc/connector/dataplane/aws/s3/TestFunctions.java
@@ -24,24 +24,27 @@ public class TestFunctions {
 
     public static final String VALID_REGION = "validRegion";
     public static final String VALID_BUCKET_NAME = "validBucketName";
+    public static final String VALID_KEY_NAME = "validKeyName";
     public static final String VALID_ACCESS_KEY_ID = "validAccessKeyId";
     public static final String VALID_SECRET_ACCESS_KEY = "validSecretAccessKey";
+    public static final String VALID_SECRET_TOKEN = "validSecretToken";
 
     public static DataAddress s3DataAddressWithCredentials() {
         return DataAddress.Builder.newInstance()
-            .type(S3BucketSchema.TYPE)
-            .keyName("aKey")
-            .property(S3BucketSchema.BUCKET_NAME, VALID_BUCKET_NAME)
-            .property(S3BucketSchema.REGION, VALID_REGION)
-            .property(S3BucketSchema.ACCESS_KEY_ID, VALID_ACCESS_KEY_ID)
-            .property(S3BucketSchema.SECRET_ACCESS_KEY, VALID_SECRET_ACCESS_KEY)
-            .build();
+                .type(S3BucketSchema.TYPE)
+                .keyName(VALID_KEY_NAME)
+                .property(S3BucketSchema.BUCKET_NAME, VALID_BUCKET_NAME)
+                .property(S3BucketSchema.REGION, VALID_REGION)
+                .property(S3BucketSchema.ACCESS_KEY_ID, VALID_ACCESS_KEY_ID)
+                .property(S3BucketSchema.SECRET_ACCESS_KEY, VALID_SECRET_ACCESS_KEY)
+                .property(S3BucketSchema.SECRET_TOKEN, VALID_SECRET_TOKEN)
+                .build();
     }
 
     public static DataAddress s3DataAddressWithoutCredentials() {
         return DataAddress.Builder.newInstance()
                 .type(S3BucketSchema.TYPE)
-                .keyName("aKey")
+                .keyName(VALID_KEY_NAME)
                 .property(S3BucketSchema.BUCKET_NAME, VALID_BUCKET_NAME)
                 .property(S3BucketSchema.REGION, VALID_REGION)
                 .build();
@@ -49,10 +52,10 @@ public class TestFunctions {
 
     public static DataFlowRequest.Builder createRequest(String type) {
         return DataFlowRequest.Builder.newInstance()
-            .id(UUID.randomUUID().toString())
-            .processId(UUID.randomUUID().toString())
-            .sourceDataAddress(createDataAddress(type).build())
-            .destinationDataAddress(createDataAddress(type).build());
+                .id(UUID.randomUUID().toString())
+                .processId(UUID.randomUUID().toString())
+                .sourceDataAddress(createDataAddress(type).build())
+                .destinationDataAddress(createDataAddress(type).build());
     }
 
     public static DataAddress.Builder createDataAddress(String type) {
