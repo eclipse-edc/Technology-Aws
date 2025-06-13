9f4a654bbb42853ebea2187ac18e218660216ff1
ndr_brt <andrea.bertagnolli@gmail.com>
2025-06-13 09:31:47 +0200

    build: update plugin snapshot repository url

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,25 +14,13 @@
 
 rootProject.name = "technology-aws"
 
-// this is needed to have access to snapshot builds of plugins
 pluginManagement {
     repositories {
-        mavenLocal()
         maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
         }
         mavenCentral()
         gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositories {
-        mavenLocal()
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-        }
-        mavenCentral()
     }
 }
 
